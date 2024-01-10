c20c5846eeb7a2e785d668e5d8862ea42cfc26cf
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-10 11:09:17 +0100

    Ajout de la connexion + SallesService

---
+++ BackEnd/Api/ApiSalleConcert/Models/Data/SalleDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiSalleConcert.Models.Data
+{
+	public class SalleDatabaseSettings
+	{
+		public string ConnectionString { get; set; } = null!;
+		public string DatabaseName { get; set; } = null!;
+		public string SallesCollectionName { get; set; } = null!;
+	}
+}
---
+++ BackEnd/Api/ApiSalleConcert/Models/Services/SallesService.cs
@@ -0,0 +1,40 @@
+﻿using ApiSalleConcert.Models.Data;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ApiSalleConcert.Models.Services
+{
+	public class SallesService
+	{
+		private readonly IMongoCollection<Salle> _salleCollection;
+
+		public SallesService(
+			IOptions<SalleDatabaseSettings> salleStoreDatabaseSettings)
+		{
+			var mongoClient = new MongoClient(
+				salleStoreDatabaseSettings.Value.ConnectionString);
+
+			var mongoDatabase = mongoClient.GetDatabase(
+				salleStoreDatabaseSettings.Value.DatabaseName);
+
+			_salleCollection = mongoDatabase.GetCollection<Salle>(
+				salleStoreDatabaseSettings.Value.SallesCollectionName);
+		}
+
+		public async Task<List<Salle>> GetAsync() =>
+			await _salleCollection.Find(_ => true).ToListAsync();
+
+		public async Task<Salle?> GetAsync(string id) =>
+			await _salleCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+		public async Task CreateAsync(Salle newBook) =>
+			await _salleCollection.InsertOneAsync(newBook);
+
+		public async Task UpdateAsync(string id, Salle updateSalle) =>
+			await _salleCollection.ReplaceOneAsync(x => x.Id == id, updateSalle);
+
+		public async Task RemoveAsync(string id) =>
+			await _salleCollection.DeleteOneAsync(x => x.Id == id);
+	}
+}
+}
