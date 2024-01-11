7b5590181f916225b09794fd12345d3bd0e3a928
Florian12671@gmail.com <Florian12671@gmail.com>
2024-01-11 16:17:59 +0100

    Recherche par mot

--- BackEnd/Api/ApiSalleConcert/Controllers/SallesController.cs
+++ BackEnd/Api/ApiSalleConcert/Controllers/SallesController.cs
@@ -30,15 +30,82 @@ namespace ApiSalleConcert.Controllers
         [HttpGet("id")]
 		public async Task<ActionResult<Salle>> Get(int id)
 		{
-			var book = await _sallesService.GetAsync(id);
+			var salle = await _sallesService.GetAsync(id);
 
-			if (book is null)
+			if (salle is null)
 			{
 				return NotFound();
 			}
 
-			return book;
+			return salle;
 		}
+
+        [HttpGet("nomRecherche")]
+        public async Task<List<SalleRecherche>> GetAllByNom(string nomRecherche)
+		{
+            List<Salle> listeSalle = await _sallesService.GetAsync();
+
+            if (listeSalle == null || listeSalle.Count == 0)
+                return _mapper.Map<List<SalleRecherche>>(listeSalle);
+
+            List<Salle> sallesRecherchees = new();
+
+            nomRecherche = nomRecherche.ToLower();
+
+            for (int i = 0; i < listeSalle.Count; i++)
+            {
+                if (!listeSalle[i].IsDelete && listeSalle[i].Nom != null && listeSalle[i].Nom!.ToLower().StartsWith(nomRecherche))
+                    sallesRecherchees.Add(listeSalle[i]);
+            }
+
+            return _mapper.Map<List<SalleRecherche>>(sallesRecherchees);
+        }
+
+        [HttpGet("villeRecherchee")]
+        public async Task<List<SalleRecherche>> GetAllByVille(string villeRecherche)
+		{
+            List<Salle> listeSalle = await _sallesService.GetAsync();
+
+            if (listeSalle == null || listeSalle.Count == 0)
+                return _mapper.Map<List<SalleRecherche>>(listeSalle);
+
+            List<Salle> sallesRecherchees = new();
+
+            villeRecherche = villeRecherche.ToLower();
+
+            for (int i = 0; i < listeSalle.Count; i++)
+            {
+                if (!listeSalle[i].IsDelete && listeSalle[i].AdresseSalle!.Ville != null && listeSalle[i].AdresseSalle!.Ville!.ToLower().StartsWith(villeRecherche))
+                    sallesRecherchees.Add(listeSalle[i]);
+            }
+
+            return _mapper.Map<List<SalleRecherche>>(sallesRecherchees);
+        }
+
+		[HttpGet("styleRecherche")]
+		public async Task<List<SalleRecherche>> GetAllByStyles(string styleRecherche)
+		{
+            List<Salle> listeSalle = await _sallesService.GetAsync();
+
+            if (listeSalle == null || listeSalle.Count == 0)
+                return _mapper.Map<List<SalleRecherche>>(listeSalle);
+
+            List<Salle> sallesRecherchees = new();
+
+            styleRecherche = styleRecherche.ToLower();
+
+            for (int i = 0; i < listeSalle.Count; i++)
+            {
+				if (listeSalle[i].Styles != null && listeSalle[i].Styles!.Count > 0)
+                foreach (string style in listeSalle[i].Styles!)
+                {
+                    if (!listeSalle[i].IsDelete && style.ToLower().StartsWith(styleRecherche))
+                        sallesRecherchees.Add(listeSalle[i]);
+                }
+            }
+
+            return _mapper.Map<List<SalleRecherche>>(sallesRecherchees);
+        }
 
 		[HttpPost]
 		public async Task<IActionResult> Post(Salle newSalles)
