41e27015369978530d4ace7a12f81a588a6eae8a
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-12 09:25:59 +0100

    Ajout du fetch GetSalleById

--- FrontEnd/JS/Controllers/GetSalleById.js
+++ FrontEnd/JS/Controllers/GetSalleById.js
@@ -1,6 +1,6 @@
 // Récup les données de L'Api (GetAllSalle)
 const fetchData = async () => {
-  await fetch("https://localhost:44371/api/Salles")
+  await fetch("https://localhost:44371/api/Salles/id?id=1")
     .then((res) => res.json())
     .then((data) => console.log(data))
     .catch((err) => console.log("Pas de GetAllSalle", err));
