094144eeaa33ec0592b6f0fad7a4821c9a1812db
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-15 11:53:34 +0100

    Récup de l'id de la salle choisi OK

--- FrontEnd/JS/Controllers/GetSalleById.js
+++ FrontEnd/JS/Controllers/GetSalleById.js
@@ -1,6 +1,10 @@
+// Récup de l'ID dans l'URL
+let params = new URLSearchParams(document.location.search);
+let id = params.get("id");
+
 // Récup les données de L'Api (GetAllSalle)
 const fetchData = async () => {
-  await fetch("https://localhost:44371/api/Salles/id?id=1")
+  await fetch(`https://localhost:44371/api/Salles/id?id=${id}`)
     .then((res) => res.json())
     .then((data) => console.log(data))
     .catch((err) => console.log("Pas de GetAllSalle", err));
