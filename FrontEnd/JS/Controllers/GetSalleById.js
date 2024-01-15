6b8fc4f1147c6f8ee8c12038ab3d0c2e6e1f6ae9
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-15 14:57:59 +0100

    Ajout du style de la page détail OK

--- FrontEnd/JS/Controllers/GetSalleById.js
+++ FrontEnd/JS/Controllers/GetSalleById.js
@@ -6,9 +6,29 @@ let id = params.get("id");
 const fetchData = async () => {
   await fetch(`https://localhost:44371/api/Salles/id?id=${id}`)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data);
+      var lat = data.adresseSalle.localisationAdresse.coordinates[0];
+      var lon = data.adresseSalle.localisationAdresse.coordinates[1];
+
+      // utiliser les coordonnées pour définir la vue de la carte
+      var map = L.map("map").setView([lat, lon], 13);
+
+      // Ajouter une couche de carte (par exemple, OpenStreetMap)
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "© OpenStreetMap contributors",
+      }).addTo(map);
+
+      // Ajouter un marqueur à la position
+      L.marker([lat, lon]).addTo(map);
+    })
     .catch((err) => console.log("Pas de GetAllSalle", err));
 };
+
+//*******************Card**************************************//
+//*************************************************************//
+//*******************Carte*************************************//
+//*************************************************************//
 
 // Pour que la fonction de l'API se lance au chargement de la page
 window.addEventListener("DOMContentLoaded", async () => {
