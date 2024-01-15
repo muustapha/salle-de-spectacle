49c194ca5aab880f121fe329e7e6f17a533f217c
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-15 17:13:29 +0100

    afpa-soir

--- FrontEnd/JS/Models/Data/Salle.js
+++ FrontEnd/JS/Models/Data/Salle.js
@@ -1,14 +1,21 @@
-class Salle
-{
-    constructor(id, nom, adresseSalle, listeStyle, listeAvis, capacite, smac, contactSalle)
-    {
-        this.Id = id;
-        this.Nom = nom;
-        this.AdresseSalle = adresseSalle;
-        this.ListeStyle = listeStyle;
-        this.ListeAvis = listeAvis;
-        this.Capacite = capacite;
-        this.Smac = smac;
-        this.ContactSalle = contactSalle;
-    }
+class Salle {
+  constructor(
+    id,
+    nom,
+    adresseSalle,
+    listeStyle,
+    listeAvis,
+    capacite,
+    smac,
+    contactSalle
+  ) {
+    this.Id = id;
+    this.Nom = nom;
+    this.AdresseSalle = adresseSalle;
+    this.ListeStyle = listeStyle;
+    this.ListeAvis = listeAvis;
+    this.Capacite = capacite;
+    this.Smac = smac;
+    this.ContactSalle = contactSalle;
+  }
 }
