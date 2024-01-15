0eb22ddb7ac8157e4280e71afdb5c24d973bf86c
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-15 16:20:29 +0100

    Implémantation de la page détail OK (non responsive)

--- FrontEnd/JS/Controllers/GetSalleById.js
+++ FrontEnd/JS/Controllers/GetSalleById.js
@@ -7,7 +7,8 @@ const fetchData = async () => {
   await fetch(`https://localhost:44371/api/Salles/id?id=${id}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      displayDescriptionSalle(data);
+      displayAvis(data);
       var lat = data.adresseSalle.localisationAdresse.coordinates[0];
       var lon = data.adresseSalle.localisationAdresse.coordinates[1];
 
@@ -24,10 +25,86 @@ const fetchData = async () => {
     })
     .catch((err) => console.log("Pas de GetAllSalle", err));
 };
+// pour afficher les styles
+const displayElementBoucle = (data) => {
+  let style = "";
+  data.forEach((element) => {
+    style += element + " ";
+  });
+  return style;
+};
 
-//*******************Card**************************************//
+//*******************Dispolay info*****************************//
+const displayDescriptionSalle = ({
+  nom,
+  adresseSalle,
+  capacite,
+  smac,
+  styles,
+}) => {
+  const nomPage = document.getElementById("salle");
+  const descriptionSalle = document.querySelectorAll("[data-adresse]");
+
+  nomPage.innerHTML = nom + " - details salle";
+
+  for (let i = 0; i < descriptionSalle.length; i++) {
+    if (descriptionSalle[i].dataset.adresse == "nom") {
+      descriptionSalle[i].innerHTML = nom;
+    }
+    if (descriptionSalle[i].dataset.adresse == "adresse") {
+      descriptionSalle[i].innerHTML =
+        adresseSalle.numero == null
+          ? adresseSalle.voie
+          : adresseSalle.numero + " " + adresseSalle.voie;
+    }
+    if (descriptionSalle[i].dataset.adresse == "code") {
+      descriptionSalle[i].innerHTML = adresseSalle.codePostal;
+    }
+    if (descriptionSalle[i].dataset.adresse == "ville") {
+      descriptionSalle[i].innerHTML = adresseSalle.ville;
+    }
+    if (descriptionSalle[i].dataset.adresse == "capacite") {
+      descriptionSalle[i].innerHTML = capacite + " Personnes";
+    }
+    if (descriptionSalle[i].dataset.adresse == "smac") {
+      if (smac == true) {
+        descriptionSalle[i].innerHTML = "Oui";
+      } else {
+        descriptionSalle[i].innerHTML = "Nom";
+      }
+    }
+    if (descriptionSalle[i].dataset.adresse == "style") {
+      descriptionSalle[i].innerHTML = displayElementBoucle(styles);
+    }
+  }
+};
 //*************************************************************//
-//*******************Carte*************************************//
+//*******************Avis*************************************//
+const displayAvis = ({ listeAvis }) => {
+  let containerAvis = document.querySelector(".container-avis");
+  if (listeAvis != null) {
+    listeAvis.forEach((a) => {
+      console.log(a.date);
+      const divAvis = document.createElement("div");
+      divAvis.classList.add("avis");
+
+      const pAvis = document.createElement("p");
+      pAvis.classList.add("avis-note");
+      pAvis.innerHTML = a.note + " / 10";
+
+      const pDate = document.createElement("p");
+      pDate.classList.add("avis-date");
+      let dateOrigine = new Date(a.date);
+      let options = { day: "2-digit", month: "2-digit", year: "numeric" };
+      pDate.innerHTML = dateOrigine.toLocaleDateString("fr-FR", options);
+
+      divAvis.appendChild(pAvis);
+      divAvis.appendChild(pDate);
+
+      containerAvis.appendChild(divAvis);
+    });
+  }
+};
 //*************************************************************//
 
 // Pour que la fonction de l'API se lance au chargement de la page
