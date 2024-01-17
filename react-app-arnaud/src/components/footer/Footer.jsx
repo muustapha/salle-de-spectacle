2f049749dc399bedd7a74301a61ec5a5f56da7bf
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-17 16:02:31 +0100

    Ajout du footer et du style OK

---
+++ react-app-arnaud/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import style from "./Footer.module.css";
+
+const Footer = () => {
+    return ( 
+    <>
+        <footer className={style.footer}>
+            <p className={style.p}>Ne pas copier, merci bien !</p>
+        </footer>
+    </> );
+}
+ 
+export default Footer;
--- react-app-arnaud/src/pages/MainPage.jsx
+++ react-app-arnaud/src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
+import Footer from "../components/footer/Footer";
 import HeaderMobile from "../components/header/mobile/HeaderMobile";
 
 const MainPage = () => {
     return (
         <>
             <HeaderMobile />
+            <Footer />
         </>
 
       );
