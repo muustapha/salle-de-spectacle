56f90a4be50b0d3d36757b94837c914c9c98ad74
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-23 13:26:24 +0100

    Ajout du style de la page AjoutSalle OK

--- react-app-arnaud/src/components/addSalle/FormAddSalle.jsx
+++ react-app-arnaud/src/components/addSalle/FormAddSalle.jsx
@@ -7,30 +7,30 @@ const FormAddSalle = () => {
             <form className={style.form}>
                 <p className={style.require}>* Champs obligatoires</p>
                 <div className={style.divColumn}>
-                    <label className={style.partie} htmlFor="name">Nom salle* :</label>
+                    <label className={style.partie} htmlFor="name">Nom salle*</label>
                     <input className={style.inputName} type="text" name="name" id="name" />
                 </div>
                 <div className={style.div}>
                     <p className={style.partie}>Adresse*</p>
-                    <div className={style.divRowCenter}>
+                    <div className={style.divColumn}>
                         <label className={style.labelNumAdresse} htmlFor="num">Numéro :</label>
-                        <input className={style.inputNumAdresse} type="text" name="" id="num" />
+                        <input  type="text" name="" id="num" />
                     </div>
-                    <div className={style.divRowCenter}>
+                    <div className={style.divColumn}>
                         <label htmlFor="voie">Voie :</label>
                         <input type="text" name="voie" id="voie" />
                     </div>
-                    <div className={style.divRowCenter}>
+                    <div className={style.divColumn}>
                         <label htmlFor="postal">Code postal :</label>
                         <input className={style.inputCodePostal} type="number" name="postal" id="postal" />
                     </div>                    
-                    <div className={style.divRowCenter}>
+                    <div className={style.divColumn}>
                         <label htmlFor="ville">Ville :</label>
                         <input type="text" name="ville" id="ville" />
                     </div>
                 </div>
                 <div>
-                    <p className={style.partie}>Localisation :</p>
+                    <p className={style.partie}>Localisation</p>
                     <div>
                         <div className={style.divSep}>
                             <label  htmlFor="coordonnées-x">Coordonnée x:</label>
@@ -43,35 +43,35 @@ const FormAddSalle = () => {
                     </div>
                 </div>
                 <div className={style.div}>
-                    <p className={style.partie}>Contact* :</p>
+                    <p className={style.partie}>Contact*</p>
                     <div  className={style.divSep}>
                         <label htmlFor="tel">Téléphone :</label>
                         <input className={style.inputCoord} type="tel" name="tel" id="tel" />
                     </div>
                 </div>
                 <div className={style.div}>
-                    <label  className={style.partie} htmlFor="capacite">Capacités* :</label>
+                    <label  className={style.partie} htmlFor="capacite">Capacités*</label>
                     <input className={style.inputTel} type="number" name="capacite" id="capacite" />
                 </div>
                 <div className={style.div}>
-                    <p className={style.partie}>SMAC* :</p>
+                    <p className={style.partie}>SMAC*</p>
                     <div className={style.divRowCenter}>
-                        <div>
-                            <label htmlFor="smac">Oui</label>
+                        <div className={style.divSepSmac}>
+                            <label className={style.labelSmac} htmlFor="smac">Oui</label>
                             <input type="radio" name="sm" id="smac" />
                         </div>
-                        <div>
-                            <label htmlFor="smac-non">Non</label>
+                        <div className={style.divSepSmac}>
+                            <label className={style.labelSmac} htmlFor="smac-non">Non</label>
                             <input type="radio" name="sm" id="smac" />
                         </div>
                     </div>
                 </div>
                 <div>
-                    <label className={style.partie} htmlFor="style">Styles* :</label>
+                    <label className={style.partie} htmlFor="style">Styles*</label>
                     <input type="text" name="style" id="style" />
                 </div>
-                <div>
-                    <button>Validé</button>
+                <div className={style.divBtn}>
+                    <button className={style.btn}>Validé</button>
                 </div>
 
             </form>
