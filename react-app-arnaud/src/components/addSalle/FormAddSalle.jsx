1f2ae1f5219605d58c6ee30181cfff32aad269ad
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-23 14:47:15 +0100

    Correction du CSS OK

--- react-app-arnaud/src/components/addSalle/FormAddSalle.jsx
+++ react-app-arnaud/src/components/addSalle/FormAddSalle.jsx
@@ -37,68 +37,68 @@ const FormAddSalle = () => {
             <form className={style.form}>
                 <p className={style.require}>* Champs obligatoires</p>
                 <div className={style.divColumn}>
-                    <label className={style.partie} htmlFor="name">Nom salle*</label>
-                    <input className={style.inputName} type="text" name="name" id="name" />
+                    <label className={`${style.partie} ${style.label}`} htmlFor="name">Nom salle*</label>
+                    <input className={`${style.inputName} ${style.input}` } type="text" name="name" id="name" />
                 </div>
                 <div className={style.div}>
                     <p className={style.partie}>Adresse*</p>
                     <div className={style.divColumn}>
-                        <label className={style.labelNumAdresse} htmlFor="num">Numéro :</label>
-                        <input  type="text" name="" id="num" />
+                        <label className={`${style.labelNumAdresse} ${style.label}`} htmlFor="num">Numéro :</label>
+                        <input className={style.input} type="text" name="" id="num" />
                     </div>
                     <div className={style.divColumn}>
-                        <label htmlFor="voie">Voie :</label>
-                        <input type="text" name="voie" id="voie" />
+                        <label className={style.label} htmlFor="voie">Voie :</label>
+                        <input className={style.input} type="text" name="voie" id="voie" />
                     </div>
                     <div className={style.divColumn}>
-                        <label htmlFor="postal">Code postal :</label>
-                        <input className={style.inputCodePostal} type="number" name="postal" id="postal" />
+                        <label className={style.label} htmlFor="postal">Code postal :</label>
+                        <input className={`${style.inputCodePostal} ${style.input}`} type="number" name="postal" id="postal" />
                     </div>                    
                     <div className={style.divColumn}>
-                        <label htmlFor="ville">Ville :</label>
-                        <input type="text" name="ville" id="ville" />
+                        <label className={style.label} htmlFor="ville">Ville :</label>
+                        <input className={style.input} type="text" name="ville" id="ville" />
                     </div>
                 </div>
                 <div>
                     <p className={style.partie}>Localisation</p>
                     <div>
                         <div className={style.divSep}>
-                            <label  htmlFor="coordonnées-x">Coordonnée x:</label>
+                            <label className={style.label}  htmlFor="coordonnées-x">Coordonnée x:</label>
                             <input className={style.inputCoord} type="text" name="coordonnées-x" id="coordonnées-x" />
                         </div>
                         <div className={style.divSep}>
-                            <label htmlFor="coordonnées-y">Coordonnée y:</label>
-                            <input className={style.inputCoord} type="text" name="coordonnées-y" id="coordonnées-y" />
+                            <label className={style.label} htmlFor="coordonnées-y">Coordonnée y:</label>
+                            <input className={`${style.inputCoord} ${style.input}`} type="text" name="coordonnées-y" id="coordonnées-y" />
                         </div>
                     </div>
                 </div>
                 <div className={style.div}>
                     <p className={style.partie}>Contact*</p>
                     <div  className={style.divSep}>
-                        <label htmlFor="tel">Téléphone :</label>
+                        <label className={style.label} htmlFor="tel">Téléphone :</label>
                         <input className={style.inputCoord} type="tel" name="tel" id="tel" />
                     </div>
                 </div>
                 <div className={style.div}>
                     <label  className={style.partie} htmlFor="capacite">Capacités*</label>
-                    <input className={style.inputTel} type="number" name="capacite" id="capacite" />
+                    <input className={`${style.inputTel} ${style.input}`} type="number" name="capacite" id="capacite" />
                 </div>
                 <div className={style.div}>
                     <p className={style.partie}>SMAC*</p>
                     <div className={style.divRowCenter}>
                         <div className={style.divSepSmac}>
-                            <label className={style.labelSmac} htmlFor="smac">Oui</label>
-                            <input type="radio" name="sm" id="smac" />
+                            <label className={`${style.labelSmac} ${style.label}`} htmlFor="smac">Oui</label>
+                            <input className={style.input} type="radio" name="sm" id="smac" />
                         </div>
                         <div className={style.divSepSmac}>
-                            <label className={style.labelSmac} htmlFor="smac-non">Non</label>
-                            <input type="radio" name="sm" id="smac-non" />
+                            <label className={`${style.labelSmac} ${style.label}`} htmlFor="smac-non">Non</label>
+                            <input className={style.input} type="radio" name="sm" id="smac-non" />
                         </div>
                     </div>
                 </div>
                 <div>
                     <label className={style.partie} htmlFor="style">Styles*</label>
-                    <input type="text" name="style" id="style" />
+                    <input className={style.input} type="text" name="style" id="style" />
                 </div>
                 <div className={style.divBtn}>
                     <button onClick={(e) => handelClick(e)} className={style.btn}>Validé</button>
