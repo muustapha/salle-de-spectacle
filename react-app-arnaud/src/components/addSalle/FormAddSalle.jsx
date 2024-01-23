65840953976ba011731ccd691cba583a494677f1
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-23 12:11:49 +0100

    Ajout du CSS FormAjoutSalle pas fini

--- react-app-arnaud/src/components/addSalle/FormAddSalle.jsx
+++ react-app-arnaud/src/components/addSalle/FormAddSalle.jsx
@@ -4,53 +4,58 @@ const FormAddSalle = () => {
     return ( 
     <>
         <section className={style.section}>
-            <form className={style.from}>
-                <div className={style.div}>
-                    <label htmlFor="name">Nome de la salle :</label>
-                    <input type="text" name="name" id="name" />
+            <form className={style.form}>
+                <p className={style.require}>* Champs obligatoires</p>
+                <div className={style.divColumn}>
+                    <label className={style.partie} htmlFor="name">Nom salle* :</label>
+                    <input className={style.inputName} type="text" name="name" id="name" />
                 </div>
                 <div className={style.div}>
-                    <p>Adresse :</p>
-                    <div>
-                        <label htmlFor="num">Numéro :</label>
-                        <input type="text" name="num" id="num" />
+                    <p className={style.partie}>Adresse*</p>
+                    <div className={style.divRowCenter}>
+                        <label className={style.labelNumAdresse} htmlFor="num">Numéro :</label>
+                        <input className={style.inputNumAdresse} type="text" name="" id="num" />
                     </div>
-                    <div>
+                    <div className={style.divRowCenter}>
                         <label htmlFor="voie">Voie :</label>
                         <input type="text" name="voie" id="voie" />
                     </div>
-                    <div>
+                    <div className={style.divRowCenter}>
                         <label htmlFor="postal">Code postal :</label>
-                        <input type="number" name="postal" id="postal" />
+                        <input className={style.inputCodePostal} type="number" name="postal" id="postal" />
+                    </div>                    
+                    <div className={style.divRowCenter}>
+                        <label htmlFor="ville">Ville :</label>
+                        <input type="text" name="ville" id="ville" />
                     </div>
+                </div>
+                <div>
+                    <p className={style.partie}>Localisation :</p>
                     <div>
-                        <p>Localisation :</p>
-                        <div>
-                            <div>
-                                <label htmlFor="coordonnées-x">Coordonnée x:</label>
-                                <input type="text" name="coordonnées-x" id="coordonnées-x" />
-                            </div>
-                            <div>
+                        <div className={style.divSep}>
+                            <label  htmlFor="coordonnées-x">Coordonnée x:</label>
+                            <input className={style.inputCoord} type="text" name="coordonnées-x" id="coordonnées-x" />
+                        </div>
+                        <div className={style.divSep}>
                             <label htmlFor="coordonnées-y">Coordonnée y:</label>
-                                <input type="text" name="coordonnées-y" id="coordonnées-y" />
-                            </div>
+                            <input className={style.inputCoord} type="text" name="coordonnées-y" id="coordonnées-y" />
                         </div>
                     </div>
                 </div>
                 <div className={style.div}>
-                    <p>Contact :</p>
-                    <div>
+                    <p className={style.partie}>Contact* :</p>
+                    <div  className={style.divSep}>
                         <label htmlFor="tel">Téléphone :</label>
-                        <input type="tel" name="tel" id="tel" />
+                        <input className={style.inputCoord} type="tel" name="tel" id="tel" />
                     </div>
                 </div>
                 <div className={style.div}>
-                    <label htmlFor="capacite">Capacités :</label>
-                    <input type="number" name="capacite" id="capacite" />
+                    <label  className={style.partie} htmlFor="capacite">Capacités* :</label>
+                    <input className={style.inputTel} type="number" name="capacite" id="capacite" />
                 </div>
                 <div className={style.div}>
-                    <p>SMAC :</p>
-                    <div>
+                    <p className={style.partie}>SMAC* :</p>
+                    <div className={style.divRowCenter}>
                         <div>
                             <label htmlFor="smac">Oui</label>
                             <input type="radio" name="sm" id="smac" />
@@ -62,8 +67,11 @@ const FormAddSalle = () => {
                     </div>
                 </div>
                 <div>
-                    <label htmlFor="style">Styles :</label>
+                    <label className={style.partie} htmlFor="style">Styles* :</label>
                     <input type="text" name="style" id="style" />
+                </div>
+                <div>
+                    <button>Validé</button>
                 </div>
 
             </form>
