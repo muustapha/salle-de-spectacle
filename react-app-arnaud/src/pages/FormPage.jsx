e11cb16468ff52e1cddd9d47fbccb7e171cc182e
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-26 11:18:11 +0100

    AFPA

--- react-app-arnaud/src/pages/FormPage.jsx
+++ react-app-arnaud/src/pages/FormPage.jsx
@@ -1,5 +1,3 @@
-import Footer from "../components/footer/Footer";
-import Header from "../components/header/Header";
 import EventForm from "../components/formEvent/FormEvent";
 
 const FormulaireEvent = () => {
