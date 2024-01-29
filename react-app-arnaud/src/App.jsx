ccc369cea76c0676e993b53714d5508bd0918f68
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2024-01-29 14:37:42 +0100

    Ajout branch Arnaud

--- react-app-arnaud/src/App.jsx
+++ react-app-arnaud/src/App.jsx
@@ -1,6 +1,23 @@
+
 import Navigation from "./layout/Navigation"
+import { UserContext } from "./components/context/UserContext";
+import { useContext, useEffect } from "react";
 
 function App() {
+
+  const { updateUserContext } = useContext(UserContext);
+
+  useEffect(() => {
+      const userRole = localStorage.getItem('UserRole');
+      const userToken = localStorage.getItem('UserToken');
+  
+      if (userRole && userToken) {
+        updateUserContext("token", userToken);
+        updateUserContext("role", userRole);
+      }
+  }, []);
+
+
 
   return (
     <>   
--- react-app-arnaud/src/main.jsx
+++ react-app-arnaud/src/main.jsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./styles/mainStyle.css"
+import UserProvider from './components/context/UserContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <UserProvider>
+        <App />
+    </UserProvider>
 )
